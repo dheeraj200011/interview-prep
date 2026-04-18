7a46db932532709688acf35fed9894324f2e7cff
Dheeraj Gupta <dheeraj200011@gmail.com>
2026-04-18 10:58:33 +0530

    redux start

---
+++ react questions/redux/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const App = () => {
+  return <div>App</div>;
+};
+
+// ques 1: what is Redux ?
+// ans: REDUX is a state management library often used with react. it helps in managing the application state in a predicatbel way by centralizing the state in a single store, enabling easy debugging and testing.
+
+export default App;
